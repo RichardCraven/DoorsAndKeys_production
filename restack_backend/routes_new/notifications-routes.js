f0e58b6370cc7637e73f961702cd630624a10fa2
Richard <rcraven85@gmail.com>
2026-08-30 19:33:07 -0700

    fixing critical error

--- restack_backend/routes_new/notifications-routes.js
+++ restack_backend/routes_new/notifications-routes.js
@@ -5,5 +5,8 @@ module.exports = app => {
     // Trigger an email notification when a user enters a dungeon
     router.post("/enter-dungeon", Notification.enterDungeon);
     
+    // Trigger an email notification when feedback is submitted
+    router.post("/feedback", Notification.sendFeedback);
+    
     app.use('/api/notifications', router);
   };
